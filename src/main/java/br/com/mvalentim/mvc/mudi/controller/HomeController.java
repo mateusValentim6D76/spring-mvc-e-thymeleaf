338d85f56895b15ca1b5fabbd15be2f995b15dad
mateusValentim6D76 <mateus.valentim19@hotmail.com>
2022-08-18 22:36:15 -0300

    Criando home controller e home.html

---
+++ src/main/java/br/com/mvalentim/mvc/mudi/controller/HomeController.java
@@ -0,0 +1,11 @@
+package br.com.mvalentim.mvc.mudi.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+
+	public String home() {
+		return "home";
+	}
+}
