3a6907c6ebae01916307b13239f2e6ce0bcfa376
Mateus Valentim de Oliveira <mateus.valentim19@hotmail.com>
2022-09-17 18:33:49 -0300

    Usando Spring Data JPA

--- src/main/java/br/com/mvalentim/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/mvalentim/mvc/mudi/controller/HomeController.java
@@ -2,28 +2,33 @@ package br.com.mvalentim.mvc.mudi.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.mvalentim.mvc.mudi.model.Pedidos;
+import br.com.mvalentim.mvc.mudi.repository.PedidosRepository;
 
 @Controller
 public class HomeController {
 
-	@PersistenceContext
-	private EntityManager entityManager;
+	@Autowired
+	private PedidosRepository pedidosRepository;
+
+//	@GetMapping("/home")
+//	public String home(Model model) {
+//		List<Pedidos> pedidos = pedidosRepository.recuperaTodosOsPedidos();
+//		model.addAttribute("pedidos", pedidos);
+//		return "home";
+//	}
 
 	@GetMapping("/home")
-	public String home(Model model) {
-
-		javax.persistence.Query query = entityManager.createQuery("select p from Pedidos p", Pedidos.class);
-		List<Pedidos> pedidos = query.getResultList();
-
-		model.addAttribute("pedidos",pedidos);
-		return "home";
+	public ModelAndView home(Model model) {
+		ModelAndView mv = new ModelAndView("home");
+		List<Pedidos> pedidos = pedidosRepository.findAll();
+		model.addAttribute("pedidos", pedidos);
+		return mv;
 	}
 }
---
+++ src/main/java/br/com/mvalentim/mvc/mudi/repository/PedidosRepository.java
@@ -0,0 +1,16 @@
+package br.com.mvalentim.mvc.mudi.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.mvalentim.mvc.mudi.model.Pedidos;
+
+@Repository
+public interface PedidosRepository extends JpaRepository<Pedidos, Long> {
+
+}
